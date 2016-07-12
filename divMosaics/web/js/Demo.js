df0d80daa24a680802174d4ad634e74a47dea200
Sam Wood <sam@sam.uk.net>
2016-07-12 15:14:01 +0100

    Refactor and format Demo.js.  Add sunset image.

--- divMosaics/web/js/Demo.js
+++ divMosaics/web/js/Demo.js
@@ -1,13 +1,85 @@
-function Demo() {
+function Demo(config) {
     var el = document.createElement('div');
     var listener = new window.keypress.Listener();
 
-    var bars = [];
-
-    var barFactory = new BarFactory(); // Provides unused, free, bar elements on demand and handles their reuse them
+    // display width and display height - ensure these are a multiple of config.resolution for best display
+    var dw = 1020, // Servo default width is 1024
+        dh = 740; // Servo default height is 740
+    
+    window.resizeTo(dw, dh);
+    
+    var bars = []; // Array to store all 
+    var barFactory = new BarFactory(); // Provides unused, free, bar elements on demand and handles their reuse
     barFactory.populate(1000); // Create 1000 bars, ready to be used
 
-    function addBar(bar) { // Adds a child to dom and bars list
+    var barWidth  = config.resolution,
+        barHeight = config.resolution;
+
+    var barsX = Math.ceil(document.body.clientWidth / barWidth), // Number of bars (tiles) across x axis
+        barsY = Math.ceil(document.body.clientHeight / barHeight); // Number of bars (tiles) across y axis
+
+    initImageSelector(config.images);
+
+    // Display a choice of images to mosaic-ify, given a list of urls
+    function initImageSelector(imageURLs) {
+
+        // Create an add an ImageSelector component for the URLs
+        var imgSelector = new ImageSelector(imageURLs);
+        el.appendChild(imgSelector.el);
+
+        imgSelector.toggle();
+        
+        // Pressing space toggles the image selector
+        listener.simple_combo('space', () => {
+            imgSelector.toggle();
+        });
+
+        // Selecting an image causes the image selector to be hidden and a mosaic of that image to be displayed
+        imgSelector.addEventListener('imageSelected', (evt) => {
+            imgSelector.toggle(() => {
+                displayMosaic(evt.detail.image);
+            });
+        });
+    }
+
+    function displayMosaic(image) {
+        // Experimental: Use canvas colour picking to get colour colour grid representing image
+        var icp = new ImageColorPicker(image);
+        var grid = icp.getColorGrid({height: barsY});
+         // Clear the previously displayed bars and paint the new grid when ready
+        resetBars(() => {
+            paint(grid);
+        });
+    }
+
+    function paint(grid) {
+        var waitTime = 1000; // Time before starting animation
+
+        // Caclulate offset required for mosaic to be displayed in centre of screen
+        var offsetX = Math.max((document.body.clientWidth - (grid[0].length * barWidth)) / 2, 0);
+        var offsetY = Math.max((document.body.clientHeight - (grid.length * barHeight)) / 2, 0);
+
+        _.times(barsY, (y) => {
+            _.times(barsX, (x) => {
+                var color = grid[y][x];
+
+                var dx = offsetX + (x * barWidth),
+                    dy = offsetY + (y * barHeight);
+
+                // Request and add a bar object from the bar factory
+                var b = barFactory.bar(_.random(document.body.clientWidth), document.body.clientHeight, barWidth, barHeight, color);
+                addBar(b);
+
+                setTimeout(() => { // Start animating the bar into place after a fixed amount of time
+                    b.tweenPos(dx, dy, 1000)
+                    .start(); 
+                }, waitTime);
+
+            });
+        });
+    }
+
+    function addBar(bar) { // Adds a to dom and bars list
         el.appendChild(bar.el);
         bars.push(bar);
     }
@@ -22,7 +94,8 @@ function Demo() {
         // Keep track of how many are done
         var nComplete = 0,
             nTotal = bars.length;
-        bars.forEach((bar, i) => {
+
+        bars.forEach((bar) => {
             // Fade out each bar
             bar.tweenPos(_.random(0, document.body.clientWidth), document.body.clientHeight, 1000)
                 .onComplete(() => {
@@ -42,117 +115,6 @@ function Demo() {
         });
     }
 
-    var resolution = 20;
-    // display width and display height - ensure these are a multiple of resolution for best display
-    var dw = 1020, // Servo default width is 1024
-        dh = 740; // Servo default height is 740
-    window.resizeTo(dw, dh);
-
-    var barWidth  = resolution,
-        barHeight = resolution;
-
-    var barsX = Math.ceil(document.body.clientWidth / barWidth),
-        barsY = Math.ceil(document.body.clientHeight / barHeight);
-
-    ((imageURLs) => {
-        // imageURLs is a list of image routes corresponding to available images to display
-        var imgSelector = new ImageSelector(imageURLs);
-        el.appendChild(imgSelector.el);
-        imgSelector.toggle();
-        listener.simple_combo('space', () => {
-            imgSelector.toggle();
-        });
-        imgSelector.addEventListener('imageSelected', (evt) => {
-            imgSelector.toggle(() => {
-                displayMosaic(evt.detail.image);
-            });
-        });
-    })(['images/banksy1.jpg', 'images/banksy2.jpg', 'images/bars.jpg', 'images/chrome.jpg','images/firefox.png', 'images/red.png','images/servo.jpg', 'images/sunset.jpg']);
-
-    function displayMosaic(image) {
-        // Experimental: Use canvas colour picking to get colour colour grid representing image
-        var icp = new ImageColorPicker(image);
-        var grid = icp.getColorGrid({height: barsY});
-        resetBars(() => {
-            paint(grid);
-        });
-
-        // Using server to get colour grid representing image
-        
-     /*   var imgUrl = image.src;
-        Http.get(imgUrl + '?c=true&width=' + barsX +'&height=' + barsY, (grid) => {
-            // Clear current display, if any
-            resetBars(() => {
-                // Once this is done, display the new mosaic
-                paint(grid);
-            });
-        }, (err) => {
-            console.error(err);
-        });*/
-    }
-
-
-    // Progress Bar to display loading progress
-    var pb = new ProgressBar(0, 0, document.body.clientWidth, 2, 0);
-    // el.appendChild(pb.el);
-
-    function paint(grid) {
-        var waitTime = 1000; // Time before starting animation
-        var zi = 1; // zIndex to assign to bar when interacted with
-
-        var calcPaintProgress = (() => { // Function to determine % of cells that have been processed
-            var nCells = grid.length * grid[0].length;
-
-            return (x, y) => {
-                var nProcessed = (y * grid[0].length) + x;
-                return (nProcessed / nCells) * 100;
-            };
-        })();
-
-        // Caclulate offset required for mosaic to be displayed in centre of screen
-        var offsetX = Math.max((document.body.clientWidth - (grid[0].length * barWidth)) / 2, 0);
-        var offsetY = Math.max((document.body.clientHeight - (grid.length * barHeight)) / 2, 0);
-        _.times(barsY, (y) => {
-            _.times(barsX, (x) => {
-                var color = grid[y][x];
-                var dx = offsetX + (x * barWidth),
-                    dy = offsetY + (y * barHeight);
-
-                var b = barFactory.bar(_.random(document.body.clientWidth), document.body.clientHeight, barWidth, barHeight, color);
-                addBar(b);
-
-                b.addEventListener('mouseover', () => {
-                    b.el.style.zIndex = zi++; // Ensure the bar is on top, so changes are visible
-                    // b.tweenOpacity(0, 500)
-                        // .chain(b.tweenOpacity(1, 500))
-                        // .start();
-                });
-
-                setTimeout(() => { // Start animating the bar into place after a fixed amount of time
-                    b.tweenPos(dx, dy, 1000)
-                    .start(); 
-                }, waitTime);
-
-                // Display progress
-                pb.set(calcPaintProgress(x, y));
-            });
-        });
-    }
-
-    function randomColorGrid(hue) {
-         var grid = [];
-        _.times(barsY, () => {
-            var row = [];
-
-            _.times(barsX, () => {
-                r.push(hue ? randomColor({hue: hue}): randomColor());
-            });
-
-            grid.push(row);
-        });
-        return grid;
-    }
-   
     this.animate = function(t) {
         TWEEN.update(t);
     };
