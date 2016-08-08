f830a99925887795914e57be14bad8e7de46c890
sole <sole@mozilla.com>
2016-08-08 16:12:15 +0100

    CSS Transform: showing more than one transform

--- experiments/test-transformations/main.js
+++ experiments/test-transformations/main.js
@@ -4,26 +4,40 @@ window.onload = function() {
 	var classes = [ 'rotate-x', 'rotate-y', 'rotate-z' ];
 	var container = document.getElementById('container');
 	var delayInc = 200;
-
-	console.log(classes.length);
+	var interval = 2000;
 	
 	for(var i = 0; i < classes.length; i++) {
 		var c = classes[i];
 
+		var wrapper = makeWrapper(c);
+
 		var el = document.createElement('div');
-		container.appendChild(el);
+
+		wrapper.appendChild(el);
+		container.appendChild(wrapper);
 
 		el.classList.add('transitions');
 
-		scheduleToggling(el, c, i * delayInc);
+		scheduleToggling(el, c, i * delayInc, interval);
 	}
 
-	function scheduleToggling(el, c, delay) {
-		console.log('schedule', el, c, delay);
+	function scheduleToggling(el, c, delay, interval) {
 		setTimeout(function() {
 			setInterval(function() {
 				el.classList.toggle(c);
-			});
+			}, interval);
 		}, delay);
+	}
+
+	function makeWrapper(title) {
+		var el = document.createElement('div');
+		el.className = 'wrapper';
+
+		var h2 = document.createElement('h2');
+		h2.innerHTML = title;
+
+		el.appendChild(h2);
+
+		return el;
 	}
 };
