48d07d8be888b1ba47d496b3aaa70f539d4ef53e
sole <sole@mozilla.com>
2016-08-09 16:07:59 +0100

    CSS Transform: add remaining transformations

--- experiments/test-transformations/main.js
+++ experiments/test-transformations/main.js
@@ -1,7 +1,7 @@
 // skew, rotate X Y Z, translate X Y Z
 
 window.onload = function() {
-	var classes = [ 'translate', 'translate-x', 'translate-y', 'translate-z', 'rotate', 'rotate-x', 'rotate-y', 'rotate-z', 'skew', 'skew-x', 'skew-y' ];
+	var classes = [ 'translate', 'translate-x', 'translate-y', 'translate-z', 'translate-3d', 'rotate', 'rotate-x', 'rotate-y', 'rotate-z', 'skew', 'skew-x', 'skew-y', 'scale', 'scale-x', 'scale-y', 'scale-3d', 'perspective', 'matrix' ];
 	var elementFactories = [
 		{ title: 'div', func: getDiv },
 		{ title: 'iframe', func: getIframe }
