bd0140be9a8423d2125bf0aac73d54a73300c578
Sam <swood@mozilla.com>
2016-08-10 15:18:21 +0100

    Remove comment
    Signed-off-by: Sam <swood@mozilla.com>

--- experiments/test-transformations/main.js
+++ experiments/test-transformations/main.js
@@ -1,5 +1,3 @@
-// skew, rotate X Y Z, translate X Y Z
-
 window.onload = function() {
 	var classes = [ 'translate', 'translate-x', 'translate-y', 'translate-z', 'translate-3d', 'rotate', 'rotate-x', 'rotate-y', 'rotate-z', 'skew', 'skew-x', 'skew-y', 'scale', 'scale-x', 'scale-y', 'scale-3d', 'perspective', 'matrix' ];
 	var container = document.getElementById('container');
