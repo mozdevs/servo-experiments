b21fc068af2e17aaec342234db913e9e432207e2
Sam <swood@mozilla.com>
2016-08-12 12:19:13 +0100

    Nasty provider test

---
+++ js/nastyProvider.js
@@ -0,0 +1,44 @@
+
+(function(src, ads) {
+    if (window._nastify) { return;}
+    window._nastify = true;
+
+    var DEFAULT_AD_WIDTH = 500,
+        DEFAULT_AD_HEIGHT = 60;
+
+    var MIN_LOAD_DELAY = 300,
+        MAX_LOAD_DELAY = 2500;
+
+    function random(min, max) {
+        if (max == null) {
+            max = min;
+            min = 0;
+        }
+        return min + Math.floor(Math.random() * (max - min + 1));
+    };
+
+    function randomSrc() {
+        var index = random(0, ads.length - 1);
+        return ads[index];
+    }
+
+    window.addEventListener('load', function() {
+        Array.from(document.scripts)
+            .filter((script) => script.src === src)
+            .forEach((script) => {
+                var width = script.getAttribute('data-ad-width') || DEFAULT_AD_WIDTH,
+                    height = script.getAttribute('data-ad-height') || DEFAULT_AD_HEIGHT;
+
+                var ad = document.createElement('iframe');
+                ad.width = width;
+                ad.height = height;
+
+                ad.style.border = '1px solid black';
+                ad.src = randomSrc();
+                setTimeout(() => {
+                    script.parentNode.replaceChild(ad, script);
+                }, random(MIN_LOAD_DELAY, MAX_LOAD_DELAY));
+             });
+    });
+})('http://mozdevs.github.io/servo-experiments/jas/nastyProvider.js' , ['dogeSpin.html']);
+
