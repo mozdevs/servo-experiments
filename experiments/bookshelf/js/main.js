acaafd36d898a8edf0b098ea1dbf2560df3ed0fa
Sam <swood@mozilla.com>
2016-08-15 17:59:12 +0100

    Refactor bookshelf to separate CSS and JS
    Signed-off-by: Sam <swood@mozilla.com>

---
+++ experiments/bookshelf/js/main.js
@@ -0,0 +1,49 @@
+(function() {
+    var noneSelected = {
+            el: document.createElement('div'),
+            active: true,
+            toggle: () => {}
+    }; // dummy 
+
+    var selected = noneSelected;
+
+    Array.from(document.querySelectorAll('.book-tab'))
+        .map((el) => {
+            var content = el.querySelector('.book-tab-content'),
+                active = false;
+
+            return {
+                toggle: function () {
+                    if (active) {
+                        el.style.width = '140px';
+                        content.style.opacity = '0';
+                        content.style.pointerEvents = 'none';
+                    } else {
+                        el.style.width = 'calc(100vw - 560px - 12px - 2px)';
+                        content.style.opacity = '1';
+                        content.style.pointerEvents = 'auto';
+
+                    }
+                    active = !active;
+                },
+                el: el
+            };
+        })
+        .forEach((tab) => {
+
+            // Animate in the book tab
+            setTimeout(() => {
+                tab.el.style.transform = 'translateX(0vw)'
+            }, 500);
+
+            tab.el.addEventListener('click', function(evt) {
+                if (selected === tab) {
+                    selected = noneSelected;
+                    return tab.toggle();
+                }   
+                selected.toggle();
+                tab.toggle();
+                selected = tab;
+        });
+    });
+})();
