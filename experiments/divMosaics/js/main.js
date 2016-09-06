40b8b720a69ea3cc9e715dd2439f2fb1897703a8
Sam Wood <sam@sam.uk.net>
2016-09-06 11:25:24 +0100

    Download local MDL
    Signed-off-by: Sam Wood <sam@sam.uk.net>

--- experiments/divMosaics/js/main.js
+++ experiments/divMosaics/js/main.js
@@ -1,9 +1,6 @@
 window.addEventListener('load', init);
 
 function init() {
-	var stats = new ServoStats();
-	document.body.appendChild(stats.dom);
-
 	var demo = new Demo({
     	resolution: 20, // Resolution is the width and height of each mosaic tile - the smaller this value, the more demanding the operation
     	images: ['images/banksy1.jpg', 'images/banksy2.jpg', // List of images that can be selected to be mosaic-ified
