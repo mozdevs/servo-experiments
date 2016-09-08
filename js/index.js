c30337e932f9921cbddd23f575af17a775b13bae
Sam Wood <sam@sam.uk.net>
2016-09-08 12:20:40 +0100

    Add descriptions for featured experiments.
    Signed-off-by: Sam Wood <sam@sam.uk.net>

--- js/index.js
+++ js/index.js
@@ -11,39 +11,54 @@ Http.get = function(url, cb) {
 
 window.addEventListener('load', function() {
     Http.get('experiments.json', function(data) {
-        addExperiments(document.querySelector('#featured-experiments .experiment-previews'), data.featured);
+        addExperiments(document.querySelector('#featured-experiments .experiment-previews'), data.featured, true);
         addExperiments(document.querySelector('#other-experiments .experiment-previews'), data.experiments);
         addExperiments(document.querySelector('#technical-tests .experiment-previews'), data.tests);
     });
 
-    var hrefWrap = (el, href) => {
-        var a = document.createElement('a');
-        a.href = href;
-        a.appendChild(el);
-        return a;
-    }
+    var tagWrap = function(tagName, el) {
+        var tagEl = document.createElement(tagName);
+        tagEl.appendChild(el);
+        return tagEl;
+    };
 
-    function addExperiments(ul, experiments) {
+    var hrefWrap = function (el, href) {
+        var a = tagWrap('a', el);
+        a.href = href;
+        return a;
+        return a;
+    };
+
+    var pWrap = tagWrap.bind(null, 'p');
+
+    function addExperiments(ul, experiments, showDesc) {
         var lis = experiments
             .map((info) => {
                 var article = document.createElement('article');
                 article.classList.add('experiment-preview');
+
                 var h3 = document.createElement('h3');
                 h3.textContent = info.name;
+                article.appendChild(hrefWrap(h3, info.href));
 
                 var screen = document.createElement('img');
                 screen.src = info.href + 'thumb.png';
                 screen.width = 256;
                 screen.height = 256;
-
-
-                var desc = document.createElement('p');
-                desc.classList.add('experiment-desc');
-                desc.textContent = info.desc;
-
-                article.appendChild(hrefWrap(h3, info.href));
                 article.appendChild(hrefWrap(screen, info.href));
-                // article.appendChild(hrefWrap(desc, info.href));
+
+                if (showDesc) {
+                    var desc = document.createElement('div');
+                    desc.classList.add('experiment-desc');
+                    if (info.desc.indexOf('<p>') === -1) { // No p tag detected, insert one.
+                        desc.appendChild(pWrap(document.createTextNode(info.desc)));
+                    } else {
+                        // Otherwise just use HTML provided.
+                        desc.innerHTML = info.desc;
+                    }
+                    article.appendChild(hrefWrap(desc, info.href));
+                }
+
                 return article;
             });
 
@@ -55,8 +70,6 @@ window.addEventListener('load', function() {
 
         // Add the preview elements to dom        
         lis.forEach((li) => ul.appendChild(li));
-
-
     }
 
     requestAnimationFrame(animate);
