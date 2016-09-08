4adfc7aafb42b3a06926bc021c612859b21cdcdd
Sam Wood <sam@sam.uk.net>
2016-09-08 14:21:28 +0100

    Rewrite mosaic gallery JS into es5 so it works in Safari
    Signed-off-by: Sam Wood <sam@sam.uk.net>

--- experiments/tiles/main.js
+++ experiments/tiles/main.js
@@ -1,22 +1,23 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', function () {
 
-    let config = getConfig();
+    var config = getConfig();
         
-    let container = document.getElementById('container');
+    var container = document.getElementById('container');
 
-    let tiles = createTiles(container, config.w, config.h, config.tileSize);
+    var tiles = createTiles(container, config.w, config.h, config.tileSize);
 
-    let images = ['havana.jpeg', 'earth.jpeg', 'london.jpeg']
-                .map((fn) => 'images/' + fn)
-                .map((src) => {
-                    let img = new Image();
+    var images = ['havana.jpeg', 'earth.jpeg', 'london.jpeg']
+                .map(function (fn){ return 'images/' + fn; })
+                .map(function (src) {
+                    var img = new Image();
                     img.src = src;
                     return img;
                 });
 
-    let imageTime = 2200; // the delay inbetween image changes.
+    var imageTime = 2200; // the delay inbetween image changes.
 
-    let {stop} = cycleArray(images, imageTime, displayImageOnTiles.bind(null, tiles));
+    var arrayCyler = cycleArray(images, imageTime, displayImageOnTiles.bind(null, tiles));
+    var stop = arrayCyler.stop;
 
     function cycleArray(arr, time, f) {
         /* 
@@ -26,9 +27,9 @@ window.addEventListener('load', () => {
             Returns object with stop function which ends the cycling.
         */
 
-        let index = 0;
+        var index = 0;
 
-        let next = () => {
+        var next = function() {
             f(arr[index]);
             index = (index + 1) % arr.length;
         };
@@ -37,61 +38,43 @@ window.addEventListener('load', () => {
         next();
 
         // Then cycle through them on a delay.
-        let interval = setInterval(next, time);
+        var interval = setInterval(next, time);
 
         return {
             stop: clearInterval.bind(window, interval)
         };
     }
 
-    tiles.iterate((tile) => {
-        let className = 'skew';
-        tile.el.addEventListener('mouseover', () => {
-            // TODO: INVESTIGATE - NEXT LINE CAUSES ERROR IN SERVO.
-            // tile.el.classList.toggle(className);
-        });
-        tile.el.addEventListener('mouseout', () => {
-            // TODO: INVESTIGATE - NEXT LINE CAUSES ERROR IN SERVO.
-            // tile.el.classList.remove(className);
-        });
-    });
-
-    function coordToTile(x, y) {
-        let tileX = Math.floor(x / config.tileSize),
-            tileY = Math.floor(y / config.tileSize);
-        return tiles.grid[tileY][tileX]; 
-    }
-
     function displayImageOnTiles(tiles, img) {
-        let display = () => {
-            let icp = new ImageColorPicker(img);
+        var display = function () {
+            var icp = new ImageColorPicker(img);
 
             // Calculate good dimensions based on tiles available, whilst maintaining aspect ratio.
-            let ratio = img.naturalWidth / img.naturalHeight;
-            let dimensions = img.naturalWidth >= img.naturalHeight ? 
+            var ratio = img.naturalWidth / img.naturalHeight;
+            var dimensions = img.naturalWidth >= img.naturalHeight ? 
                             {width:  tiles.w, height: Math.floor(tiles.w / ratio)} 
                             : {width: Math.floor(tiles.h * ratio), height: tiles.h};
            
-            let colorGrid = icp.getColorGrid(dimensions);
+            var colorGrid = icp.getColorGrid(dimensions);
 
-            tiles.iterate((tile, x, y) => {
-                let colorStr;
+            tiles.iterate(function (tile, x, y) {
+                var colorStr;
                 if (colorGrid[y] && colorGrid[y][x]) { // If colour grid specifies a colour then use that.
-                    let {r, g, b, a} = colorGrid[y][x];
-                    colorStr = `rgba(${r}, ${g}, ${b}, ${a})`;
+                    var color = colorGrid[y][x];
+                    colorStr = 'rgba(' + color.r + ', ' + color.g + ', ' + color.b + ', ' + color.a + ')';
                 } else {
-                    // Otherwise use black colour
+                    // Otherwise use black colour.
                     colorStr = 'black';
                 }
 
-                // Delay the colour change based on the row number
+                // Delay the colour change based on the row number.
                 setTimeout(
                     (function(t, colorStr) {
-                        return () => {
+                        return function() {
                             t.setColor(colorStr);
                         };
                     })(tile, colorStr),
-                   (y * 20) + (x * 10));
+                   (y * 20) + (x * 10)); // This function of y and x determines the effect when transitioning between different images.
             });
         };
         if (img.naturalWidth) { // If image loaded then display it immediately.
@@ -102,12 +85,12 @@ window.addEventListener('load', () => {
     }
 
     function createTiles(container, totalWidth, totalHeight, tileSize) {
-        let tiles = [];
+        var tiles = [];
 
-        for (let y = 0; y < totalHeight; y += tileSize) {
-            let row = [];
-            for (let x = 0; x < totalWidth; x += tileSize) {
-                let tile = new Tile(x, y);
+        for (var y = 0; y < totalHeight; y += tileSize) {
+            var row = [];
+            for (var x = 0; x < totalWidth; x += tileSize) {
+                var tile = new Tile(x, y);
                 container.appendChild(tile.el);
                 row.push(tile);
             }
@@ -121,12 +104,12 @@ window.addEventListener('load', () => {
             el.style.width = tileSize + 'px';
             el.style.height = tileSize + 'px';
             
-            this.setPosition = (x, y) => {
+            this.setPosition = function (x, y) {
                 el.style.left = x + 'px';
                 el.style.top = y + 'px';
             };
 
-            this.setColor = (color) => {
+            this.setColor = function (color) {
                 setTimeout(function() {
                     el.style.backgroundColor = color;
                 }, 200);
@@ -138,11 +121,11 @@ window.addEventListener('load', () => {
 
         return {
             grid: tiles, // Give access to underlying array.
-            iterate: (cb) => { // A function for iteration through all the tiles.
-                let yMax = tiles.length,
+            iterate: function(cb) { // A function for iteration through all the tiles.
+                var yMax = tiles.length,
                     xMax = tiles[0].length;
-                for (let y = 0; y < yMax; y++) {
-                    for (let x = 0; x < xMax; x++) {
+                for (var y = 0; y < yMax; y++) {
+                    for (var x = 0; x < xMax; x++) {
                         cb(tiles[y][x], x, y); // Pass the callback the tile and the position.
                     }
                 }
@@ -154,10 +137,10 @@ window.addEventListener('load', () => {
     }
 
     function getConfig(windowSize) {
-        let width = window.innerWidth,
+        var width = window.innerWidth,
             height = window.innerHeight;
 
-        let tileSize = calcTileSize(width, height);
+        var tileSize = calcTileSize(width, height);
 
         return {
             tileSize: tileSize,
